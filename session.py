bc243cafedf5996db6413a5c497657ae583e71fd
jjensen <jjensen@malwarebytes.com>
2021-05-19 14:57:02 -0400

    Keep session alive

--- session.py
+++ session.py
@@ -1,6 +1,7 @@
 import threading
 import websockets
 import time
+import asyncio
 
 class SessionManager:
 
@@ -9,11 +10,12 @@ class SessionManager:
         self._ip = ip
         self._port = port
 
-    def create_session(self, recreate=False):
+    async def create_session(self, recreate=False):
         if recreate or not self._session_id:
-            socket = websockets.connect("ws://%s:%i" % (self._ip, self._port))
-            socket.send('{"handshake_req":{"version":"3.1.1.3","browser":"Firefox 88.0","js_lib_version":"3.1.1.3","installer_key":"qSytHg7BcJ","env":"production"}}')
-            session = socket.recv()
-            print(session)
+            # socket = websockets.connect("ws://%s:%i" % (self._ip, self._port))
+            async with websockets.connect("ws://%s:%i" % (self._ip, self._port)) as websocket:
+                websocket.send('{"handshake_req":{"version":"3.1.1.3","browser":"Firefox 88.0","js_lib_version":"3.1.1.3","installer_key":"qSytHg7BcJ","env":"production"}}')
+                session = websocket.recv()
+                print(session)
 
             
